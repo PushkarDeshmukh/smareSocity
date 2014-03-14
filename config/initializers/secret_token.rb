c73811408207ebd39cb5694193622f56aad60f12
Pushkar <pushkardeshmukh@hotmail.com>
2014-03-14 16:35:14 +0530

    complete Devise sign_in sign_up page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartsoc::Application.config.secret_key_base = '173e5569099513935927cdf37137cf985a3d649cc27cbb931e1105a5a2770c0e7670d79b97aa3d0c601a9e8099222e95b834d39151a5f0243ce2b88fab407c70'
